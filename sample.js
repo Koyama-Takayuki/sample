566888ccc534ff13abf26c5a29384e8341bc3230
tkyk-k <tkyk.koyama@gmail.com>
2025-03-29 19:48:55 +0900

    Update sample.js

--- sample.js
+++ sample.js
@@ -3,7 +3,7 @@ function setup() {
 }
 
 function draw() {
-    background(framaCount%256);
+    background(framaCount%256);aaa
     let x = frameCount%width;
     let y = height/2 + sin(frameCount*0.1)*50;
     ellipse(x, y, 20, 20);
