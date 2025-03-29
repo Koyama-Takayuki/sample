06aab5b558fcc3d25d3d993a06952927bfd42485
tkyk-k <tkyk.koyama@gmail.com>
2025-03-29 21:21:22 +0900

    Update sample.js

--- sample.js
+++ sample.js
@@ -1,13 +1,16 @@
+let speed = 0.1;
+
 function setup() {
-    createCanvas(400, 400);
+    createCanvas(800, 600);
 }
 
 function draw() {
     background(0);
 
-    fill(255, 0, 0);
+    speed = map(mouseX, 0, width, 0.01, 0.3);
     let x = frameCount%width;
-    let y = height/2 + sin(frameCount*0.1)*50;
+    let y = height/2 + sin(frameCount*speed)*100;
+    fill(255, 0, 0);
     ellipse(x, y, 20, 20);
 
     fill(100, 200, 200);
